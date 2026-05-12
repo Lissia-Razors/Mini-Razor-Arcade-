ff321d0f32f9b7ac4ac518e4532bc6968ee17ea0
Lissia-Razors <ellen839@gmail.com>
2026-05-12 12:19:33 -0400

    Add files via upload

---
+++ index.html/script.js
@@ -0,0 +1,6 @@
+result.innerHTML += `<br><br><button onclick="shareResult()">Send this to a friend ??</button>`;
+
+function shareResult() {
+  alert("Send this to someone who needs a shave ??");
+}
+</html>
