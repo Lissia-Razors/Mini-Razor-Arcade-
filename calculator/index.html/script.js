b2eb74cf781a71d4c696eba3de291a6420501a87
Lissia-Razors <ellen839@gmail.com>
2026-05-12 12:19:08 -0400

    Delete calculator/index.html directory

--- calculator/index.html/script.js
+++
@@ -1,6 +0,0 @@
-result.innerHTML += `<br><br><button onclick="shareResult()">Send this to a friend ??</button>`;
-
-function shareResult() {
-  alert("Send this to someone who needs a shave ??");
-}
-</html>
